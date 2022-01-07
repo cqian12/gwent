6af368513e25f740c3b231e19197f1becfdbae51
Charles Qian <charleshqian@gmail.com>
2022-01-07 12:45:56 -0800

    added basic elements

---
+++ front-end/src/components/AddGame.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddGame = () => {
+    return (
+        <div>
+            <h1>This will be the form to add data</h1>
+        </div>
+    )
+}
+
+export default AddGame
---
+++ front-end/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Landing = () => {
+    return (
+        <div className='Landing'> 
+            <div>
+                <h1>Gwent Tracker</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
